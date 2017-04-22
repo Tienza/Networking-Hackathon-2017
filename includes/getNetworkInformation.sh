e0346573c26ba935d74dcc4e84180d562308b486
Piradon Liengtiraphan <Piradon.Liengtiraphan1@gmail.com>
2017-04-22 12:18:09 -0400

    Finished Auto Insert to db with python

--- includes/getNetworkInformation.sh
+++ includes/getNetworkInformation.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-command_result=$(iwlist wlp4s0 scanning)
+command_result=$(iwlist wlan1 scanning)
 
-/home/graham/getAddress.sh "$command_result"
-/home/graham/getFrequency.sh "$command_result"
-/home/graham/getQuality.sh "$command_result"
-/home/graham/getSignal.sh "$command_result"
-/home/graham/getSSID.sh "$command_result"
+/var/www/html/includes/getAddress.sh "$command_result"
+/var/www/html/includes/getFrequency.sh "$command_result"
+/var/www/html/includes/getQuality.sh "$command_result"
+/var/www/html/includes/getSignal.sh "$command_result"
+/var/www/html/includes/getSSID.sh "$command_result"
---
+++ includes/python_parse.py
@@ -0,0 +1,51 @@
+import re
+import MySQLdb
+
+debug = False
+
+db = MySQLdb.connect(host="localhost",    # your host, usually localhost
+                     user="root",         # your username
+                     passwd="FredFredBurger",  # your password
+                     db="networkData")        # name of the data base
+
+cur = db.cursor()
+
+def chunks(array, splitVal):
+    for i in xrange(0, len(array), splitVal):
+        yield array[i:i + splitVal]
+
+def main():
+    print("Begin Parse...")
+    with open('log.txt', 'r') as file:
+        text = file.read()
+        macAddr = re.findall('(\w+:\w+:\w+:\w+:\w+:\w+)',text)
+        with open('log.txt','r') as file2:
+            text2 = file2.readlines()       
+            text2 = [x.strip() for x in text2]    
+            text2 = list(chunks(text2,len(macAddr)))
+            if debug:
+                print(text2)
+                for i in text2:
+                    print(str(len(i)))
+        
+        if debug:
+            for value in xrange(0, len(text2[0])):
+                print(str(text2[0][value]) + ', ' + str(text2[1][value]) + ', ' + str(text2[2][value]) + ', ' + str(text2[3][value]) + ', ' + str(text2[4][value]))
+    return text2
+
+networks = main()
+
+print(str(networks))
+
+# Use all the SQL you like
+for network in xrange(0, len(networks[0])):
+    print("Inserting...")
+    query = 'INSERT INTO Network(SSID, MacAddr, Frequency, Quality, Strength) VALUES("' + str(networks[4][network]) + '", "' + str(networks[0][network]) + '", "' + str(networks[1][network]) + '", "' + str(networks[2][network]) + '", "' + str(networks[3][network])+ '");'
+    print(query)
+    try:
+        cur.execute(query)
+        db.commit()
+    except:
+        db.rollback()
+
+db.close()

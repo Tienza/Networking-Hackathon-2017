8287a5adf6ecfc641f4e60b19ec6b7c3bcf338c0
Graham <graham.burek1@gmail.com>
2017-04-22 02:30:47 -0400

    Adds Haskell database file.

---
+++ includes/database.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Database.MySQL.Simple
+import Data.List
+import Data.List.Split
+
+main = do
+  file <- getContents
+  let numLines = length $ lines file
+  let dataList = chunksOf (numLines `div` 5) (lines file)
+  conn <- connect defaultConnectInfo 
+    { connectUser = "root",
+      connectPassword = "FredFredBurger",
+      connectDatabase = "networkData"
+    }
+  mapM_ (insertInto stmt conn) (transpose dataList) 
+
+
+stmt :: Query
+stmt = "insert into Networks (SSID, MacAddr, Frequency, Quality, SignalStrength) values (?,?,?,?,?)" 
+
+insertInto :: Query -> Connection -> [String] -> IO ()
+insertInto stmt conn xs = do
+  execute conn stmt [(xs !! 0),(xs !! 1),(xs !! 2),(xs !! 3),(xs !! 4)]
+  close conn
